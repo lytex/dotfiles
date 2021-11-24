e5d97fedadb73992f889d11c188cb835834b80af
lytex <julianlpc15@gmail.com>
2021-11-24 22:41:44 +0100

    add minified script

---
+++ minified_remote.sh
@@ -0,0 +1,2 @@
+if command -v curl > /dev/null 2>&1; then GET_CMD='curl -fL'; elif command -v wget > /dev/null 2>&1; then GET_CMD='wget -q -O -'; elif command -v fetch > /dev/null 2>&1; then GET_CMD='fetch -q -o -'; fi; if [ -z "$GET_CMD" ]; then echo 'No external command is available. Please install curl, wget, or fetch!'; exit 1; fi; eval "$GET_CMD 'https://raw.githubusercontent.com/lytex/doom.d/master/work_example.el'"
+
