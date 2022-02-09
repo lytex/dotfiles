cd5d47387f8477c5bbcac4eab039d065b576797f
lytex <julianlpc15@gmail.com>
2022-02-10 00:05:27 +0100

    one shot config

--- qmk_firmware/keyboards/contra/keymaps/lytex/config.h
+++ qmk_firmware/keyboards/contra/keymaps/lytex/config.h
@@ -35,3 +35,5 @@
 //#define MIDI_TONE_KEYCODE_OCTAVES 2
 
 #define TAPPING_TOGGLE 2
+#define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 1000  /* Time (in ms) before the one shot key is released */
