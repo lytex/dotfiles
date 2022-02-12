f1e27ee2a640656fed3bd9e6c7edbe15ae10951a
lytex <julianlpc15@gmail.com>
2022-02-12 17:44:56 +0100

    force nkro, oneshot timeout = 500ms

--- qmk_firmware/keyboards/contra/keymaps/lytex/config.h
+++ qmk_firmware/keyboards/contra/keymaps/lytex/config.h
@@ -36,4 +36,5 @@
 
 #define TAPPING_TOGGLE 2
 #define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
-#define ONESHOT_TIMEOUT 1000  /* Time (in ms) before the one shot key is released */
+#define ONESHOT_TIMEOUT 500  /* Time (in ms) before the one shot key is released */
+#define FORCE_NKRO
