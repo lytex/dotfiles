b0dd927d861fa55e6fcd63d0b1d2df66862bd378
lytex <julianlpc15@gmail.com>
2022-09-30 22:19:56 +0200

    add command to unpin all packages

--- dot_config/nvim/utils/update_pinned.sh
+++ dot_config/nvim/utils/update_pinned.sh
@@ -5,3 +5,6 @@ find ~/.local/share/nvim/site/pack/packer -iname .git -exec bash -c 'cd "{}"; cd
 
 # Oneliner to assign a commit to all packages (run once when packages are not pinned)
 find ~/.local/share/nvim/site/pack/packer -iname .git -exec bash -c 'cd "{}"; cd ..; git remote -v | grep fetch | sed -E "s#^origin\thttps://github.com/##;s#(\.git)?[ \t]+\(fetch\)#,$(git --no-pager log -1 --pretty=format:%H)#"' \; | xargs -I _ bash -c 'var="_"; sed "s|\"${var%%,*}\"|\"${var%%,*}\", commit = \"${var##*,}\"|" -i ~/.config/nvim/lua/user/plugins.lua'
+
+# Oneliner to unpine all dependencies
+sed -Ei 's/commit = "[^"]+",?//' ~/.config/nvim/lua/user/plugins.lua
