408f5d0c2bdc6e0d09f4fe0fcc9a6c351227e34f
lytex <julianlpc15@gmail.com>
2022-11-12 03:00:38 +0100

    remap SPC b → SPC b b

--- dot_config/nvim/lua/user/whichkey.lua
+++ dot_config/nvim/lua/user/whichkey.lua
@@ -97,8 +97,11 @@ local opts = {
 local mappings = {
 	["a"] = { "<cmd>Alpha<cr>", "Alpha" },
 	["b"] = {
-		"<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<cr>",
-		"Buffers",
+		name = "buffer/bookmark",
+		b = {
+			"<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<cr>",
+			"Buffers",
+		},
 	},
 	["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
 	-- ["w"] = { "<cmd>w!<CR>", "Save" },
