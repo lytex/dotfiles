111cf878bd2810a279d5a5470280c86ab9f9e867
lytex <julianlpc15@gmail.com>
2022-12-30 17:31:55 +0100

    add air40 keymap

---
+++ qmk_firmware/keyboards/ymdk/ymd40/air40/keymaps/lytex/executable_rules.mk
@@ -0,0 +1,8 @@
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+COMBO_ENABLE = yes
+
+RGBLIGHT_SUPPORTED = no
+BACKLIGHT_SUPPORTED = no
