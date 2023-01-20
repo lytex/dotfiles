45439e80c35447e838ee4e27cd0a4baec11c77de
lytex <julianlpc15@gmail.com>
2023-01-20 19:29:12 +0100

    add persistent folds and cursor

--- dot_config/nvim/lua/user/autocommands.lua
+++ dot_config/nvim/lua/user/autocommands.lua
@@ -1,3 +1,35 @@
+-- https://github.com/NickP-real/.dotfile/blob/main/.config/nvim/lua/core/autocmd.lua
+
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+-- Persistent Folds
+local save_fold = augroup("Persistent Folds", { clear = true })
+autocmd("BufWinLeave", {
+	pattern = "*.*",
+	callback = function()
+		vim.cmd.mkview()
+	end,
+	group = save_fold,
+})
+autocmd("BufWinEnter", {
+	pattern = "*.*",
+	callback = function()
+		vim.cmd.loadview({ mods = { emsg_silent = true } })
+	end,
+	group = save_fold,
+})
+
+-- Persistent Cursor
+autocmd("BufReadPost", {
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
+
 vim.cmd([[
   augroup _general_settings
     autocmd!
