c9dd264a1e861834c7d913cb9196fde58a334948
lytex <julianlpc15@gmail.com>
2023-01-22 13:53:14 +0100

    add gwenview

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ fuse2 bluez bluez-utils docker plantuml
 alsa-utils # alsamixer, aplay
 emacs-nativecomp
 man cronie libreoffice-fresh fd xorg-xinput xorg-xkill ntfs-3g
-calibre
+calibre gwenview
 firefox chromium
 qmk
 xdg-desktop-portal xdg-desktop-portal-kde # Add GTK_USE_PORTAL=1 to /etc/environment
