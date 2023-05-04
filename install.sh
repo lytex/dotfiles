c0311b50447f81a801ae28a2de9700333e946f32
lytex <julianlpc15@gmail.com>
2023-05-04 15:39:09 +0200

    add gcloud kubernetes auth login

--- install.sh
+++ install.sh
@@ -60,7 +60,7 @@ samsung-unified-driver system-config-printer # Printer driver and config
 qmk-udev-rules-git # QMK
 )
 if (( $WORK_ENV == 1 )); then
-    paru_packages+=(google-cloud-cli teams onedrive-abraunegg)
+    paru_packages+=(google-cloud-cli google-cloud-cli-gke-gcloud-auth-plugin teams onedrive-abraunegg)
 fi
 
 paru -S "${paru_packages[@]}" --noconfirm
