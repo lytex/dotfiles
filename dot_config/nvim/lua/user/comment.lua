642b152fb6eed2a70095ec3be4a922abc8451734
lytex <julianlpc15@gmail.com>
2024-03-05 22:23:11 +0100

    add C-/ mapping

--- dot_config/nvim/lua/user/comment.lua
+++ dot_config/nvim/lua/user/comment.lua
@@ -6,3 +6,17 @@ end
 comment.setup({
 	pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 })
+
+local api = require("Comment.api")
+-- Toggle lines (linewise) using C-/
+vim.keymap.set("n", "<C-/>", api.toggle.linewise.current)
+local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+vim.keymap.set("v", "<C-/>", function()
+	vim.api.nvim_feedkeys(esc, "nv", false)
+	api.locked("toggle.linewise")(vim.fn.visualmode())
+end)
+vim.keymap.set("x", "<C-/>", function()
+	vim.api.nvim_feedkeys(esc, "nx", false)
+	api.locked("toggle.linewise")(vim.fn.visualmode())
+	vim.api.nvim_feedkeys("gv", "nx", true)
+end)
