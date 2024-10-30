79c4b01800bd6d9b58d57f681988fcf72b09c33e
lytex <julianlpc15@gmail.com>
2024-10-30 16:23:40 +0100

    alpha.nvim change buttons

--- dot_config/nvim/lua/user/alpha.lua
+++ dot_config/nvim/lua/user/alpha.lua
@@ -19,11 +19,19 @@ dashboard.section.header.val = {
 	[[    .         ':                                                                  ]],
 }
 dashboard.section.buttons.val = {
+	dashboard.button(
+		"p",
+		"  Find project",
+		"<cmd>lua require('telescope').extensions.project.project{display_type = 'full'}<cr>"
+	),
 	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
 	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
 	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+	dashboard.button(
+		"t",
+		"  Find text",
+		"<CMD>lua require('telescope.builtin').live_grep({debounce = 100, theme= 'ivy'})<CR>"
+	),
 	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
 	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
