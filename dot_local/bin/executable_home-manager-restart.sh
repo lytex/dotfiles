09c8ea08769a34375767bec1bf605d6d9fc8f67f
lytex <julianlpc15@gmail.com>
2026-02-23 08:46:37 +0100

    fix home-manager-restart?

--- dot_local/bin/executable_home-manager-restart.sh
+++ dot_local/bin/executable_home-manager-restart.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-sleep 300
-set -o pipefail
-/nix/var/nix/profiles/default/bin/nix-shell "<home-manager>" -A install
+sleep 0
 i=0
 status=0
 while (( ! status ))
 do sleep 1
     i=$((i+1))
+    /nix/var/nix/profiles/default/bin/nix-shell "<home-manager>" -A install
     echo retry $i
-    "$HOME/.nix-profile/bin/home-manager" switch 2>&1 | grep -i failed
+    output=$("$HOME/.nix-profile/bin/home-manager" switch 2>&1)
     status=$?
-    echo "status=$status"
-    if (( status != 1)); then
+    echo $output
+    echo "status home-manager = $status"
+    if (( status == 0 )); then
+        echo $output | grep -i failed
+        status=$?
+        echo "status grep = $status"
+    else
         status=0
     fi
+    if (( status == 0 )); then
+        systemctl --user status "activitywatch-watcher-awatcher.service" | grep failed 
+        status=$?
+        echo "status grep 2 = $status"
+    else
+        status=0
+    fi
+
 done
