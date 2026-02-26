967476c20917e33ab630d46920358fd60d867b5a
lytex <julianlpc15@gmail.com>
2026-02-26 20:48:06 +0100

    finally home-manager restart works

--- dot_local/bin/executable_home-manager-restart.sh
+++ dot_local/bin/executable_home-manager-restart.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
-sleep 0
+sleep 300
 i=0
 status=0
-while (( ! status ))
-do sleep 1
+while (( status == 0)); do
+    sleep 1
     i=$((i+1))
     /nix/var/nix/profiles/default/bin/nix-shell "<home-manager>" -A install
     echo retry $i
     output=$("$HOME/.nix-profile/bin/home-manager" switch 2>&1)
-    status=$?
+    status_home_manager=$?
     echo $output
-    echo "status home-manager = $status"
-    if (( status == 0 )); then
-        echo $output | grep -i failed
-        status=$?
-        echo "status grep = $status"
+    echo $output | grep -i failed
+    home_manager_failed=$?
+    sleep 5
+    systemctl --user status "activitywatch-watcher-awatcher.service" | grep failed 
+    aw1_failed=$?
+    systemctl --user status "activitywatch-watcher-aw-watcher-afk.service" | grep failed 
+    aw2_failed=$?
+    echo "status_home_manager=$status_home_manager"
+    echo "home_manager_failed=$home_manager_failed"
+    echo "aw1_failed=$aw1_failed"
+    echo "aw2_failed=$aw2_failed"
+    if(( status_home_manager == 0 && home_manager_failed == 1 && aw1_failed == 1 && aw2_failed == 1)); then
+        status=1
     else
         status=0
     fi
-    if (( status == 0 )); then
-        systemctl --user status "activitywatch-watcher-awatcher.service" | grep failed 
-        status=$?
-        echo "status grep 2 = $status"
-    else
-        status=0
-    fi
-
+    echo "status=$status"
 done
